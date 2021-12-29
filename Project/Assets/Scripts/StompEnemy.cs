c5561bdfdff66eef54ebaff908abf60c628d20b8
Kareem530 <57565666+Kareem530@users.noreply.github.com>
2021-12-29 17:32:28 +0200

    111

---
+++ Project/Assets/Scripts/StompEnemy.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StompEnemy : MonoBehaviour {
+	public GameObject enemy;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            enemy.GetComponent<Enemy>().TakeDamage(1);
+        }
+    }
+}
